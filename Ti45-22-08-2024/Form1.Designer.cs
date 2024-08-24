785e6fa4a4152dcdb3325f2149fb56dfe2378b32
marcoscozatti <marcoscozatti@gmail.com>
2024-08-23 22:17:54 -0300

    Atualizações do CRUD e Datagridview

--- Ti45-22-08-2024/Form1.Designer.cs
+++ Ti45-22-08-2024/Form1.Designer.cs
@@ -33,11 +33,16 @@
             this.btnExcluir = new System.Windows.Forms.Button();
             this.btnFechar = new System.Windows.Forms.Button();
             this.lblMensagem = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.dg = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).BeginInit();
             this.SuspendLayout();
             // 
             // btnInserir
             // 
-            this.btnInserir.Location = new System.Drawing.Point(63, 20);
+            this.btnInserir.Location = new System.Drawing.Point(29, 11);
             this.btnInserir.Name = "btnInserir";
             this.btnInserir.Size = new System.Drawing.Size(75, 23);
             this.btnInserir.TabIndex = 0;
@@ -47,7 +52,7 @@
             // 
             // btnAlterar
             // 
-            this.btnAlterar.Location = new System.Drawing.Point(63, 62);
+            this.btnAlterar.Location = new System.Drawing.Point(127, 12);
             this.btnAlterar.Name = "btnAlterar";
             this.btnAlterar.Size = new System.Drawing.Size(75, 23);
             this.btnAlterar.TabIndex = 1;
@@ -57,7 +62,7 @@
             // 
             // btnExcluir
             // 
-            this.btnExcluir.Location = new System.Drawing.Point(63, 106);
+            this.btnExcluir.Location = new System.Drawing.Point(224, 12);
             this.btnExcluir.Name = "btnExcluir";
             this.btnExcluir.Size = new System.Drawing.Size(75, 23);
             this.btnExcluir.TabIndex = 2;
@@ -67,7 +72,7 @@
             // 
             // btnFechar
             // 
-            this.btnFechar.Location = new System.Drawing.Point(63, 147);
+            this.btnFechar.Location = new System.Drawing.Point(319, 12);
             this.btnFechar.Name = "btnFechar";
             this.btnFechar.Size = new System.Drawing.Size(75, 23);
             this.btnFechar.TabIndex = 3;
@@ -78,17 +83,51 @@
             // lblMensagem
             // 
             this.lblMensagem.AutoSize = true;
-            this.lblMensagem.Location = new System.Drawing.Point(27, 189);
+            this.lblMensagem.Location = new System.Drawing.Point(26, 201);
             this.lblMensagem.Name = "lblMensagem";
             this.lblMensagem.Size = new System.Drawing.Size(28, 13);
             this.lblMensagem.TabIndex = 4;
             this.lblMensagem.Text = ".......";
             // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(29, 74);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(270, 20);
+            this.txtID.TabIndex = 5;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(29, 116);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(365, 20);
+            this.txtNome.TabIndex = 6;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(29, 161);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(365, 20);
+            this.txtTelefone.TabIndex = 7;
+            // 
+            // dg
+            // 
+            this.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg.Location = new System.Drawing.Point(29, 217);
+            this.dg.Name = "dg";
+            this.dg.Size = new System.Drawing.Size(365, 211);
+            this.dg.TabIndex = 8;
+            this.dg.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dg_CellContentClick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(203, 223);
+            this.ClientSize = new System.Drawing.Size(422, 454);
+            this.Controls.Add(this.dg);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtID);
             this.Controls.Add(this.lblMensagem);
             this.Controls.Add(this.btnFechar);
             this.Controls.Add(this.btnExcluir);
@@ -96,6 +135,8 @@
             this.Controls.Add(this.btnInserir);
             this.Name = "Form1";
             this.Text = "CRUD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,6 +149,10 @@
         private System.Windows.Forms.Button btnExcluir;
         private System.Windows.Forms.Button btnFechar;
         private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.DataGridView dg;
     }
 }
 
--- Ti45-22-08-2024/Form1.cs
+++ Ti45-22-08-2024/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -23,10 +24,12 @@ namespace Ti45_22_08_2024
                                 Persist Security Info=True;User ID=sa;Password=senac2016;";
                                 
         private string strSql = string.Empty;
-
+       
+        SqlDataAdapter da = null;
+        
         private void btnInserir_Click(object sender, EventArgs e)
         {
-            strSql = "insert into tblAgenda (nome, telefone) values ('Marcos','11986566715')";
+            strSql = "insert into tblAgenda (nome, telefone) values ('" + txtNome.Text +"','" + txtTelefone.Text + "')";
 
             sqlCon = new SqlConnection(strCon);
             SqlCommand comando = new SqlCommand(strSql, sqlCon);
@@ -36,11 +39,23 @@ namespace Ti45_22_08_2024
                 comando.ExecuteNonQuery();
                 MessageBox.Show("Registro adicionado");
             lblMensagem.Text = "Registro adicionado";
+
+            atualizagrid();
+            limpadados();
+
+        }
+
+        private void limpadados()
+        {
+            txtID.Clear();
+            txtNome.Clear();
+            txtTelefone.Clear();
+            txtNome.Focus();
         }
 
         private void btnAlterar_Click(object sender, EventArgs e)
         {
-            strSql = "update tblAgenda set nome='Marcos Cozatti', telefone='11223344' where id=8";
+            strSql = "update tblAgenda set nome='" + txtNome.Text + "', telefone='" + txtTelefone.Text + "' where id=" + txtID.Text;
             sqlCon = new SqlConnection(strCon);
             SqlCommand comando = new SqlCommand(strSql, sqlCon);
 
@@ -48,6 +63,9 @@ namespace Ti45_22_08_2024
             comando.ExecuteNonQuery();
             MessageBox.Show("Registro atualizado");
             lblMensagem.Text = "Registro atualizado";
+
+            atualizagrid();
+
         }
 
         private void btnExcluir_Click(object sender, EventArgs e)
@@ -58,7 +76,7 @@ namespace Ti45_22_08_2024
             }
             else
             {
-                strSql = "delete from tblAgenda where id=8";
+                strSql = "delete from tblAgenda where id=" + txtID.Text;
                 sqlCon = new SqlConnection(strCon);
                 SqlCommand comando = new SqlCommand(strSql, sqlCon);
 
@@ -67,11 +85,56 @@ namespace Ti45_22_08_2024
                 MessageBox.Show("Registro apagado");
                 lblMensagem.Text = "Registro apagado";
             }
+
+            atualizagrid();
+            limpadados();
         }
 
         private void btnFechar_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            atualizagrid();
+
+
+        }
+
+     
+        private void atualizagrid()
+        {
+         
+            string strSql = "SELECT * FROM tblAgenda";
+
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(strCon))
+                {
+                    SqlDataAdapter da = new SqlDataAdapter(strSql, sqlCon);
+                    DataTable dt = new DataTable();
+
+                    // Preenche o DataTable com os dados
+                    da.Fill(dt);
+
+                    // Atribui o DataTable como fonte de dados para o DataGridView
+                    dg.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao atualizar a grade: " + ex.Message);
+            }
+
+        }
+
+        private void dg_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtID.Text = dg.CurrentRow.Cells[0].Value.ToString();
+            txtNome.Text = dg.CurrentRow.Cells[1].Value.ToString();
+            txtTelefone.Text = dg.CurrentRow.Cells[2].Value.ToString();
+
         }
     }
 }
